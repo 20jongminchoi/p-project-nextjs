8227ae1bf93a20b9c78857d45bcebca0d4239d92
Your Name <you@example.com>
2024-12-15 21:18:17 +0900

    to be continued

--- app/parking/[space]/page.tsx
+++ app/parking/[space]/page.tsx
@@ -13,15 +13,17 @@ export default async function Page({
     "주차 가능 여부를 보려면 새로고침 하세요";
   const imageUrl: string | null =
     (await redis.get(cacheKeys.parkingData[space].imageUrl)) ?? null;
-  const confidence: string | null =
-    (await redis.get(cacheKeys.parkingData[space].confidence)) ?? null;
+    const confidence: number =
+    parseFloat((await redis.get(cacheKeys.parkingData[space].confidence)) ?? "0");
+  
 
+  const formattedConfidence = (confidence * 100).toFixed(3); // 89.934
   return (
     <div className="flex flex-col items-center mt-4">
       <p className="mb-2">최근 새로고침 시간: {lastUpdated}</p>
       <RefreshButton receiver={space + "receiver"} />
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <p className="mb-2">현재 이미지 분석에 대한 신뢰도는 {confidence}% 입니다.</p>
+      <p className="mb-2">현재 이미지 분석에 대한 신뢰도는 {formattedConfidence}% 입니다.</p>
       <img
         src={imageUrl ?? "https://placehold.co/960x540/png"}
         alt="parking-image"
